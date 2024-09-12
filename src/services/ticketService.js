af9cbfecbee767052cd5e09b1f9984840ddb7012
Robert Mack <robertmack123123@gmail.com>
2024-09-12 15:42:55 -0500

    added claim and close button

--- src/services/ticketService.js
+++ src/services/ticketService.js
@@ -1,5 +1,27 @@
 export const getAllTickets = () => {
-  return fetch(
+  const data = fetch(
     "http://localhost:8088/serviceTickets?_embed=employeeTickets"
-  ).then((res) => res.json());
+  );
+  const output = data.then((res) => res.json());
+  return output;
+};
+
+export const assignTicket = (employeeTicket) => {
+  return fetch(`http://localhost:8088/employeeTickets`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(employeeTicket),
+  });
+};
+
+export const updateTicket = (ticket) => {
+  return fetch(`http://localhost:8088/serviceTickets/${ticket.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(ticket),
+  });
 };
