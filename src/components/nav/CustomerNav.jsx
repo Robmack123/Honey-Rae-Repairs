dd90b7130a4c16ac731787521623ac8442932325
Robert Mack <robertmack123123@gmail.com>
2024-09-17 15:28:05 -0500

    added conditional routes

--- src/components/nav/CustomerNav.jsx
+++ src/components/nav/CustomerNav.jsx
@@ -1,24 +1,12 @@
-import "./NavBar.css";
 import { useNavigate } from "react-router-dom";
+import "./NavBar.css";
 import { Link } from "react-router-dom";
 
-export const NavBar = () => {
+export const CustomerNav = () => {
   const navigate = useNavigate();
 
   return (
     <ul className="navbar">
-      <li className="navbar-item">
-        <Link to="/tickets">Tickets</Link>
-      </li>
-      <li className="navbar-item">
-        <Link to="/employees">Employees</Link>
-      </li>
-      <li className="navbar-item">
-        <Link to="/customers">Customers</Link>
-      </li>
-      <li className="navbar-item">
-        <Link to="/profile">Profile</Link>
-      </li>
       {localStorage.getItem("honey_user") ? (
         <li className="navbar-item navbar-logout">
           <Link
